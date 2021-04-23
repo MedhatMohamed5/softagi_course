71e95cf97577259130588eff8bce511c13bfbb67
Medhat Mohamed <35978271+MedhatMohamed5@users.noreply.github.com>
2021-04-23 02:25:34 +0200

    Add bottom navigation bar, and initialize screens

---
+++ lib/layout/home_layout.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_flutter/modules/archived_tasks/archived_tasks_screen.dart';
+import 'package:udemy_flutter/modules/done_tasks/done_tasks_screen.dart';
+import 'package:udemy_flutter/modules/new_tasks/new_task_screen.dart';
+
+class HomeLayout extends StatefulWidget {
+  const HomeLayout({Key key}) : super(key: key);
+
+  @override
+  _HomeLayoutState createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int currentIndex = 0;
+
+  List<Widget> screens = [
+    NewTasksScreen(),
+    DoneTasksScreen(),
+    ArchivedTasksScreen(),
+  ];
+
+  List<String> titles = [
+    'New Tasks',
+    'Done Tasks',
+    'Archived Tasks',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          titles[currentIndex],
+        ),
+      ),
+      body: screens[currentIndex],
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Tasks'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.check_circle_outlined), label: 'Done'),
+          BottomNavigationBarItem(icon: Icon(Icons.archive), label: 'Archived'),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/modules/new_tasks/new_task_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NewTasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'New Tasks',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+      ),
+    );
+  }
+}
